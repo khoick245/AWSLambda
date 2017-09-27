8416f21db3de9a17b8325d06abe141c5d284b251
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2017-09-26 21:44:45 -0700

    finish bodypart lambda function

---
+++ fitness/src/main/java/fitnessapp/Request.java
@@ -0,0 +1,13 @@
+package fitnessapp;
+
+public class Request {
+	private String partname;
+
+	public String getPartname() {
+		return partname;
+	}
+
+	public void setPartname(String partname) {
+		this.partname = partname;
+	}
+}
