162b066422f598d6df9a18c601a2aa3a421cb4f0
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2017-09-28 10:38:00 -0700

    try to test simple java lambda with codepipeline

---
+++ Return.java
@@ -0,0 +1,13 @@
+package automation;
+
+public class Return {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
